401428ff008abd5ce259673e9b5bef5429b98f5e
pat killean <pat.killean@gmail.com>
2024-12-11 16:40:35 -0400

    pom template

--- src/dev/cljs/cache/dev.clj
+++ src/dev/cljs/cache/dev.clj
@@ -12,11 +12,25 @@
 
 (defn clean [] (b/delete {:path "target"}))
 
+(defn- pom-template [version]
+  [[:description "cljs.cache"]
+   [:url "https://github.com/pkpkpk/cljs-cache"]
+   [:licenses
+    [:license
+     [:name "MIT"]
+     [:url "https://mit-license.org"]]]
+   [:scm
+    [:url "https://github.com/pkpkpk/cljs-cache"]
+    [:connection "scm:git:https://github.com/pkpkpk/cljs-cache.git"]
+    [:developerConnection "scm:git:ssh:git@github.com:pkpkpk/cljs-cache.git"]
+    [:tag (str "v" version)]]])
+
 (def pom {:src-dirs  ["src/main"]
           :class-dir class-dir
           :lib       lib
           :version   version
-          :basis     basis})
+          :basis     basis
+          :pom-data (pom-template version)})
 
 (defn jar []
   (b/write-pom pom)
@@ -26,6 +40,8 @@
           :jar-file jar-file}))
 
 (defn deploy []
+  (assert (some? (System/getenv "CLOJARS_USERNAME")))
+  (assert (some? (System/getenv "CLOJARS_PASSWORD")))
   (dd/deploy {:artifact jar-file
               :installer :remote
               :pom-file (b/pom-path pom)}))
